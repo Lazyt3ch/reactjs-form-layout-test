c1fc5915ca7afd01c8087a1ee2ee6a59f43bfc7e
Rashid <Lazyt3ch@gmail.com>
2021-01-27 14:48:15 +0500

    working on Select

---
+++ src/Components/Select/Select.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Select.css";
+
+function Select(props) {
+    const {invalidMessage, onChangeHandler} = props;
+    const {labelText, defaultText, options, inputId } = props.data;
+  
+    return (
+      <div className="registration-form-input margin-left margin-top">
+        <label htmlFor={inputId}>
+          {labelText}
+        </label>
+  
+        <select>
+            
+        </select>
+  
+        <div className="invalid-message">
+          {/* TODO: Add message here */}
+          {invalidMessage}
+        </div>
+      </div>
+    )
+  }
+  
+  export default Select;
