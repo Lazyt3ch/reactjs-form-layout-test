10b1e6a619cb59d8d0cabb355681e9a95d7b2dd6
Rashid <Lazyt3ch@gmail.com>
2021-01-27 12:54:57 +0500

    working on Input

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -2,8 +2,17 @@ import React from 'react';
 import "./Input.css";
 
 function Input(props) {
+  const {labelText, placeholderText, onChangeHandler} = props;
+
   return (
     <div className="registration-form-input">
+      <label>
+        {labelText}
+        <input type="text" 
+          onChange={onChangeHandler}
+          placeholder={placeholderText}>
+        </input>
+      </label>
     </div>
   )
 }
