3672f7c44ccbd17a936e19dcdd2869fdbf8805f8
Rashid <Lazyt3ch@gmail.com>
2021-01-27 11:32:28 +0500

    moved Registration files to Registration folder; imported Registration in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Registration from './Components/Registration/Registration.js';
 
 function App() {
   return (
     <div className="App">
-      Add a form here...
+      <Registration />
     </div>
   );
 }
---
+++ src/Components/Registration/Registration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Registration(props) {
+  return (
+    <form className="registration">
+      
+
+    </form>
+  )
+}
+
+export default Registration;
