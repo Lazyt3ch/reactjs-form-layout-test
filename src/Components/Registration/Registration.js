e98142aa9ffc1d0f8f85851cf44480a4b58ce839
Rashid <Lazyt3ch@gmail.com>
2021-01-27 12:47:42 +0500

    create files for InputField

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -5,10 +5,10 @@ function Registration(props) {
   return (
     <div className="registration-form-container">
       <form action="" className="registration-form">
-        <div className="registration-form-header">
+        <div className="registration-form-header margin-left">
           <p className="registration-form-title">Регистрация</p>
         </div>
-        <div className="registration-form-text">
+        <div className="registration-form-text margin-left">
           <span>Уже есть аккаунт?</span> <a href="#">Войти</a>
 
 
