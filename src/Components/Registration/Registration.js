99b57afdb9ad3b46832270442f63cb7e078c7adb
Rashid <Lazyt3ch@gmail.com>
2021-01-27 12:06:23 +0500

    working on form

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -3,13 +3,13 @@ import "./Registration.css";
 
 function Registration(props) {
   return (
-    <>
+    <div className="registration-form-container">
       <form action="" className="registration-form">
         <div className="registration-form-header">
           <p className="registration-form-title">Регистрация</p>
         </div>
       </form>
-    </>
+    </div>
   )
 }
 
