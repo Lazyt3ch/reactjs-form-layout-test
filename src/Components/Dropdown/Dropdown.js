da40abb0b80ded970a80c517e1ba4e0cc8b9de7f
Rashid <Lazyt3ch@gmail.com>
2021-01-28 16:36:57 +0500

    change Dropdown

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -25,14 +25,22 @@ function Dropdown(props) {
   
   return (
     <div className="registration-form-dropdown margin-left margin-top">
-      <div>
+      <div className="registration-form-dropdown-label">
         {labelText}
       </div>      
 
       <div className="registration-form-dropdown-box"
         onClick={openDropdown}
       >
-        {initialValue}
+        <span>
+          {initialValue}
+        </span>
+
+        <span>
+          <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="-7" y="-11" width="30" height="30" fill="#0880AE"/>
+          </svg>
+        </span>
       </div>
     </div>
   )
