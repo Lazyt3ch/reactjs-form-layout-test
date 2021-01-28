2da749971c60bb833794d24931a575d6a35c8ec1
Rashid <Lazyt3ch@gmail.com>
2021-01-28 11:22:33 +0500

    change Submit

--- src/Components/Submit/Submit.js
+++ src/Components/Submit/Submit.js
@@ -5,7 +5,7 @@ function Submit(props) {
   const {termsAccepted} = props;     
   
   const getSubmitClasses = () => 
-    `registration-form-submit ${termsAccepted 
+    `registration-form-submit margin-left ${termsAccepted 
       ? "registration-form-submit-enabled"
       : "registration-form-checkbox-disabled"}`;
   
