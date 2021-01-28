fe1278897ce870ac8940a9ace9c7f04eca2a9e34
Rashid <Lazyt3ch@gmail.com>
2021-01-28 12:07:17 +0500

    change validators

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -6,7 +6,12 @@ const validate = (value, valueType) => {
       const ENGLISH_LETTERS = "abcdefghijklmnopqrstuvwxyz";
       const SOME_SPANISH_LETTERS = "áéíóúüñ";
       const usernameChars = `${RUSSIAN_LETTERS}${ENGLISH_LETTERS}${SOME_SPANISH_LETTERS} -`;
-      return Array.from(value.toLowerString()).every( char => usernameChars.includes(char) );
+      return Array.from(value.toLowerCase()).every( char => usernameChars.includes(char) );
+    case "email":
+      // Regex source: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s01.html
+      return /^[a-z0-9+_.-]+@[a-z0-9.-]+$/i.test(value);
+    case "phone":
+      return true;
     default:
       return true;
   }
