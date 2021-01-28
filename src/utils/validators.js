2504064e4917b0a4fbb45d79b621a421480762f1
Rashid <Lazyt3ch@gmail.com>
2021-01-28 11:57:30 +0500

    change username validation

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,12 +1,11 @@
 const validate = (value, valueType) => {
-  // Username
-  // I'm not sure what to do with Chinese characters...
-  const ENGLISH_LETTERS = "abcdefghijklmnopqrstuvwxyz";
-  const RUSSIAN_LETTERS = "абвгдеёжзиклмнопрстуфхцчшщъюьэюя";
-  const usernameChars = `${ENGLISH_LETTERS}${RUSSIAN_LETTERS} -`;
-
   switch (valueType) {
     case "username":
+      // I'm not sure what to do with Chinese characters...
+      const RUSSIAN_LETTERS = "абвгдеёжзиклмнопрстуфхцчшщъюьэюя";
+      const ENGLISH_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+      const SOME_SPANISH_LETTERS = "áéíóúüñ";
+      const usernameChars = `${RUSSIAN_LETTERS}${ENGLISH_LETTERS}${SOME_SPANISH_LETTERS} -`;
       return Array.from(value.toLowerString()).every( char => usernameChars.includes(char) );
     default:
       return true;
