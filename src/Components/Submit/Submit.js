bf22eb0874f92cb4a5840fc44571f783757032c4
Rashid <Lazyt3ch@gmail.com>
2021-01-28 11:40:39 +0500

    change Submit

--- src/Components/Submit/Submit.js
+++ src/Components/Submit/Submit.js
@@ -2,10 +2,10 @@ import React from 'react';
 import "./Submit.css";
 
 function Submit(props) {
-  const {termsAccepted} = props;     
+  const {isSubmitReady} = props;     
   
   const getSubmitClasses = () => 
-    `registration-form-submit margin-left ${termsAccepted 
+    `registration-form-submit margin-left ${isSubmitReady 
       ? "registration-form-submit-enabled"
       : "registration-form-submit-disabled"}`;
   
