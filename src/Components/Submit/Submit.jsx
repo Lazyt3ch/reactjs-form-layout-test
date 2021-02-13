f9b40e5bd6a7e5662e092cae0967fadf6d6c2cf9
Rashid <Lazyt3ch@gmail.com>
2021-02-13 11:24:09 +0500

    working on Submit button styling

--- src/Components/Submit/Submit.jsx
+++ src/Components/Submit/Submit.jsx
@@ -4,13 +4,14 @@ import "./Submit.css";
 function Submit(props) {
   const {isSubmitReady, handleSubmit} = props;     
   
-  const getSubmitClasses = () => 
-    `registration-form__submit registration-form_margin-left ${isSubmitReady 
-      ? "registration-form__submit_enabled"
-      : "registration-form__submit_disabled"}`;
+  // const getSubmitClasses = () => 
+  //   `registration-form__submit registration-form_margin-left ${isSubmitReady 
+  //     ? "registration-form__submit_enabled"
+  //     : "registration-form__submit_disabled"}`;
   
+  // className={getSubmitClasses()}
   return (
-    <button type="button" className={getSubmitClasses()}
+    <button type="button" className="registration-form__submit registration-form_margin-left"
       disabled={!isSubmitReady}
       onClick={handleSubmit}
       tabIndex={6}
