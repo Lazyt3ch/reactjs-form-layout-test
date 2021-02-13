ab529f57c5bc3857edb93ead36bc5e876fe2d260
Rashid <Lazyt3ch@gmail.com>
2021-02-13 15:30:32 +0500

    add space and enter keyup handling for Submit

--- src/Components/Submit/Submit.jsx
+++ src/Components/Submit/Submit.jsx
@@ -3,12 +3,20 @@ import "./Submit.css";
 
 function Submit(props) {
   const {isSubmitReady, handleSubmit, tabIndex} = props;     
+
+  const handleKeyUp = (e) => {
+    if (e.keyCode === 32 || e.keyCode === 13) { // Space or Enter
+      // console.log("Space or Enter");
+      handleSubmit();
+    }
+  }
   
   return (
     <button type="button" 
       className="registration-form__submit registration-form_margin-left"
       disabled={!isSubmitReady}
       onClick={handleSubmit}
+      onKeyUp={handleKeyUp}
       tabIndex={tabIndex}
     >
       Зарегистрироваться
