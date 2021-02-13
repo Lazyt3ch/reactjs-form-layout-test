61856545d1288413e3edbaa4f33015721b37d591
Rashid <Lazyt3ch@gmail.com>
2021-02-13 13:43:10 +0500

    clean up Submit

--- src/Components/Submit/Submit.jsx
+++ src/Components/Submit/Submit.jsx
@@ -4,14 +4,9 @@ import "./Submit.css";
 function Submit(props) {
   const {isSubmitReady, handleSubmit} = props;     
   
-  // const getSubmitClasses = () => 
-  //   `registration-form__submit registration-form_margin-left ${isSubmitReady 
-  //     ? "registration-form__submit_enabled"
-  //     : "registration-form__submit_disabled"}`;
-  
-  // className={getSubmitClasses()}
   return (
-    <button type="button" className="registration-form__submit registration-form_margin-left"
+    <button type="button" 
+      className="registration-form__submit registration-form_margin-left"
       disabled={!isSubmitReady}
       onClick={handleSubmit}
       tabIndex={6}
