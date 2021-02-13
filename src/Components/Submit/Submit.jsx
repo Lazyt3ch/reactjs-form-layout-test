01704d1e73211bf60ce29f4cd3c2578556a77805
Rashid <Lazyt3ch@gmail.com>
2021-02-13 16:06:50 +0500

    working on Submit button

--- src/Components/Submit/Submit.jsx
+++ src/Components/Submit/Submit.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Submit.css";
 
 function Submit(props) {
   const {isSubmitReady, handleSubmit, tabIndex} = props;     
 
-  const handleKeyUp = (e) => {
-    if (e.keyCode === 32 || e.keyCode === 13) { // Space or Enter
+  const [isPressed, setIsPressed] = useState(false);
+
+  // Workaround, mostly for this 20-years-old Firefox bug: 
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=68851
+  const handleKeyDown = (e) => {
+    if (!isPressed && (e.keyCode === 32 || e.keyCode === 13)) { // Space or Enter
       // console.log("Space or Enter");
-      handleSubmit();
+      setIsPressed(true);
+      handleSubmit();      
+    }
+  };
+
+  const handleKeyUp = (e) => {
+    if (isPressed) {
+      setIsPressed(false);
     }
   }
   
@@ -15,9 +26,11 @@ function Submit(props) {
     <div className="registration-form__submit__wrapper">
       <button type="button" 
         className="registration-form__submit registration-form_margin-left"
+        style={isPressed ? { left: "2px", top: "2px" } : null}
         disabled={!isSubmitReady}
         onClick={handleSubmit}
         onKeyUp={handleKeyUp}
+        onKeyDown={handleKeyDown}
         tabIndex={tabIndex}
       >
         Зарегистрироваться
